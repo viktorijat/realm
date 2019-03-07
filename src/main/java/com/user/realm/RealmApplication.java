e5a97bbececd90e673286c1a95e2cd5c08b4459c
Viktorija Tosheva <viktorija.tosheva@tipico.com>
2019-03-07 15:13:31 +0100

    docker-compose

--- src/main/java/com/user/realm/RealmApplication.java
+++ src/main/java/com/user/realm/RealmApplication.java
@@ -4,13 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+
 @SpringBootApplication
 public class RealmApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RealmApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RealmApplication.class, args);
+    }
 }
-
-//public class RealmApplication extends SpringBootServletInitializer {
-
--- src/main/java/com/user/realm/controller/RealmController.java
+++ src/main/java/com/user/realm/controller/RealmController.java
@@ -1,18 +1,19 @@
 package com.user.realm.controller;
 
-import com.user.realm.exceptions.RealmException;
 import com.user.realm.model.Realm;
 import com.user.realm.model.RealmError;
 import com.user.realm.service.RealmService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Optional;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/user/realm")
@@ -21,21 +22,13 @@ public class RealmController {
     @Autowired
     RealmService realmService;
 
-    @RequestMapping("/")
-    public String home() {
-        return "Spring boot is working!";
-    }
-
     @GetMapping
     public List<Realm> list() {
         return realmService.getAllRealms();
     }
 
     @PostMapping
-    public ResponseEntity<?> create(HttpEntity<String> httpEntity) {
-
-        HttpHeaders n = httpEntity.getHeaders();
-        System.out.println("HEADERS: " + n.toString());
+    public ResponseEntity create(HttpEntity<String> httpEntity) {
 
         Object optional = realmService.saveOrUpdateRealm(httpEntity.getBody()).get();
         if (optional instanceof RealmError) {
@@ -46,7 +39,7 @@ public class RealmController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> get(@PathVariable("id") Integer id) {
+    public ResponseEntity get(@PathVariable("id") Integer id) {
 
         Object optional = realmService.getRealmRealmById(id).get();
         if (optional instanceof RealmError) {
